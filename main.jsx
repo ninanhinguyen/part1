7fb19db4db1540616aaada8f63764410016f27a1
ninanhinguyen <nhi.u.nguyen0111@gmail.com>
2024-07-29 22:46:49 +0300

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client'
-import App from './App'
+//import React from 'react';
+//import ReactDOM from 'react-dom/client'
+//import App from './App'
 
-const elem = document.getElementById('123')
-ReactDOM.createRoot(elem).render(<App/>)
+//const elem = document.getElementById('123')
+//ReactDOM.createRoot(elem).render(<App/>)
  
-
+import ReactDOM from 'react-dom/client'
+import App from '.App'
+ReactDOM.creatRoot(document.getElementById('root'))render(<App>/)
